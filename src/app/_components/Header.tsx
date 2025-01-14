ccb881a03c24430d7555f6b6581d7a88d6a98946
kou911 <kotaf321@gmail.com>
2025-01-14 21:28:48 +0900

    commit

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightFromBracket,
+  faDoorOpen,
+  faDove,
+  faFish,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { supabase } from "@/utils/supabase"; // ◀ 追加
+import { useAuth } from "@/app/_hooks/useAuth"; // ◀ 追加
+import { useRouter } from "next/navigation"; // ◀ 追加
+
+const Header: React.FC = () => {
+  // ▼ 追加
+  const router = useRouter();
+  const { isLoading, session } = useAuth();
+  const logout = async () => {
+    await supabase.auth.signOut();
+    router.replace("/");
+  };
+  // ▲ 追加
+
+  return (
+    <header>
+      <div className="bg-slate-800 py-2">
+        <div
+          className={twMerge(
+            "mx-4 max-w-2xl md:mx-auto",
+            "flex items-center justify-between",
+            "text-lg font-bold text-white"
+          )}
+        >
+          <div>
+            <Link href="/">
+              <FontAwesomeIcon icon={faDove} className="mr-1" />
+              MyBlogApp
+            </Link>
+          </div>
+          <div className="flex gap-x-6">
+            {/* ▼ 追加 */}
+            {!isLoading &&
+              (session ? (
+                <button onClick={logout}>
+                  <FontAwesomeIcon
+                    icon={faArrowRightFromBracket}
+                    className="mr-1"
+                  />
+                  Logout
+                </button>
+              ) : (
+                <Link href="/login">
+                  <FontAwesomeIcon icon={faDoorOpen} className="mr-1" />
+                  Login
+                </Link>
+              ))}
+            {/* ▲ 追加 */}
+            <Link href="/about">
+              <FontAwesomeIcon icon={faUser} className="mr-1" />
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/_types/PostApiResponse.ts
@@ -0,0 +1,14 @@
+export type PostApiResponse = {
+  id: string;
+  title: string;
+  content: string;
+  coverImageURL: string;
+  createdAt: string;
+  updatedAt: string;
+  categories: {
+    category: {
+      id: string;
+      name: string;
+    };
+  }[];
+};
